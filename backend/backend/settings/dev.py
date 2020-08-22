861418fff67d09ce98154d7fdc55f24c5e930890
Ayooq <imtheinvictus1@gmail.com>
2020-08-22 15:53:36 +0600

    Add Wagtail project

---
+++ backend/backend/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '$x7pjxlxu0+w@)4n!wd3stui)rx_o=dyby^y5^m(5u&ak_#187'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
